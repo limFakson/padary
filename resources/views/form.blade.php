4cc13b1fcb0d703a3a0539c8b03636bf740bd97f
limfakson <fakeyejoshua2005@gmail.com>
2024-06-23 02:20:27 +0100

    added a view and tailwind

---
+++ resources/views/form.blade.php
@@ -0,0 +1,8 @@
+@extends('index')
+<div>
+    <h3>This is the form to post to the database</h3>
+    <form action="">
+        <label for="name">Name</label>
+        <input type="text" name="name">
+    </form>
+</div>
